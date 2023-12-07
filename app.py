1647a1fd1fa8a4d671d268199b1eac04b93b9b04
sachintha180 <sachinthasenanayake180@gmail.com>
2023-12-07 18:15:28 +0530

    Add submit and exit buttons to login.html

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
