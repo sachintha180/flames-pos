0f8ff9e908fd6429789700d31d1e2e63d5eb5a4d
sachintha180 <sachinthasenanayake180@gmail.com>
2023-12-08 08:52:16 +0530

    Complete POST request handling for login.html

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
+import pymssql
 
 app = Flask(__name__)
 
-@app.route("/login")
+MSSQL_USER = "SwiftCabApp"
+MSSQL_USERPASS = "67Syjg:D4^uS"
+MSSQL_DB = "SwiftDB"
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        username, password = request.json["username"], request.json["password"]
+        # with mssql.connect("localhost", MSSQL_USER, MSSQL_USERPASS, MSSQL_DB) as conn:
+        #     with conn.cursor() as cursor:
+        #         cursor.execute("SELECT LocationName FROM LOCATIONS", )
+        #         locations = cursor.fetchall()
+        return render_template("login.html")
---
+++ static/login.js
@@ -0,0 +1,45 @@
+async function authenticate(username, password) {
+    try {
+        const response = await fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        });
+        const result = await response.json();
+        return result
+    }
+    catch (error) {
+        return error
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const submitBtn = document.querySelector("#submit")
+    const exitBtn = document.querySelector("#exit")
+    const errorLbl = document.querySelector("#error-lbl")
+
+    submitBtn.addEventListener("click", () => {
+
+        const username = document.querySelector("#username").value
+        const password = document.querySelector("#password").value
+
+        if (username && password) {
+            const response = authenticate(username, password);
+            console.log(response);
+        }
+        else {
+            if (!username) {
+                errorLbl.innerHTML = "Please enter a valid username."
+            }
+            else {
+                errorLbl.innerHTML = "Please enter a valid password."
+            }
+        }
+    })
+})
