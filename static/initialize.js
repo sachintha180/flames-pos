075d1c32bbae65f9c72fa040d390141129c9a991
sachintha180 <sachinthasenanayake180@gmail.com>
2023-12-21 09:25:28 +0530

    Refactor event listener functions

--- static/initialize.js
+++ static/initialize.js
@@ -28,13 +28,76 @@ async function addAdmin(username, password, fullname, mobile_no) {
                 username: username,
                 password: password,
                 fullname: fullname,
-                mobile_no: mobile_no
+                mobile_no: mobile_no,
             }),
         });
         const result = await response.json();
         return result;
     } catch (error) {
         return error;
+    }
+}
+
+function handleAuth() {
+    const errorTitle = document.querySelector("#error_title");
+    const errorLbl = document.querySelector("#error_lbl");
+    errorTitle.innerHTML = "";
+    errorLbl.innerHTML = "";
+
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+
+    if (username && password) {
+        authenticate(username, password).then((response) => {
+            errorTitle.innerHTML = response.message;
+            errorLbl.innerHTML = response.action;
+            if (response.data.flag) {
+                showConfirmForm(username, response.data.admin_default);
+                document
+                    .querySelector("#add_admin")
+                    .addEventListener("click", handleAdmin);
+            }
+        });
+    } else {
+        if (!username) {
+            errorTitle.innerHTML = "Invalid username";
+            errorLbl.innerHTML = "Please enter a valid username";
+        } else {
+            errorTitle.innerHTML = "Invalid password";
+            errorLbl.innerHTML = "Please enter a valid password";
+        }
+    }
+}
+function handleAdmin() {
+    const errorTitle = document.querySelector("#error_title");
+    const errorLbl = document.querySelector("#error_lbl");
+    errorTitle.innerHTML = "";
+    errorLbl.innerHTML = "";
+
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+    const fullname = document.querySelector("#fullname").value;
+    const mobile_no = document.querySelector("#mobile_no").value;
+
+    if (username && password && fullname && mobile_no) {
+        addAdmin(username, password, fullname, mobile_no).then((response) => {
+            errorTitle.innerHTML = response.message;
+            errorLbl.innerHTML = response.action;
+        });
+    } else {
+        if (!username) {
+            errorTitle.innerHTML = "Invalid username";
+            errorLbl.innerHTML = "Please enter a valid username";
+        } else if (!password) {
+            errorTitle.innerHTML = "Invalid password";
+            errorLbl.innerHTML = "Please enter a valid password";
+        } else if (!fullname) {
+            errorTitle.innerHTML = "Invalid fullname";
+            errorLbl.innerHTML = "Please enter a valid fullname";
+        } else {
+            errorTitle.innerHTML = "Invalid mobile no";
+            errorLbl.innerHTML = "Please enter a valid mobile no";
+        }
     }
 }
 
@@ -52,60 +115,12 @@ function showConfirmForm(username, admin_default) {
             <input type="text" id="fullname" name="fullname" value='${admin_default.name}'required />
             <label for="password">Mobile No</label>
             <input type="text" id="mobile_no" name="mobile_no" value='${admin_default.mobile_no}' required />
-            <input type="button" id="create_admin" name="create_admin" value="Create New Admin" />
-            <input type="button" id="clear_db" name="clear_db" value="Clear DB" />
+            <input type="button" id="add_admin" name="add_admin" value="Add New Admin" />
+            <input type="button" id="clear_db" name="clear_db" value="Clear Database" />
         </form>
     `;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const errorTitle = document.querySelector("#error_title");
-    const errorLbl = document.querySelector("#error_lbl");
-
-    const submitBtn = document.querySelector("#submit");
-    submitBtn.addEventListener("click", () => {
-        const username = document.querySelector("#username").value;
-        const password = document.querySelector("#password").value;
-
-        if (username && password) {
-            authenticate(username, password).then((response) => {
-                errorTitle.innerHTML = response.message;
-                errorLbl.innerHTML = response.action;
-                if (response.data.flag) {
-                    showConfirmForm(username, response.data.admin_default);
-                }
-            });
-        } else {
-            if (!username) {
-                errorTitle.innerHTML = "Invalid username";
-                errorLbl.innerHTML = "Please enter a valid username";
-            } else {
-                errorTitle.innerHTML = "Invalid password";
-                errorLbl.innerHTML = "Please enter a valid password";
-            }
-        }
-    });
-
-    const createBtn = document.querySelector("#create");
-    createBtn.addEventListener("click", () => {
-        const username = document.querySelector("#username").value;
-        const password = document.querySelector("#password").value;
-        const fullname = document.querySelector("#fullname").value;
-        const mobile_no = document.querySelector("#mobile_no").value;
-        
-        if (username && password && fullname && mobile_no) {
-            addAdmin(username, password, fullname, mobile_no).then((response) => {
-                errorTitle.innerHTML = response.message;
-                errorLbl.innerHTML = response.action;
-            });
-        } else {
-            if (!username) {
-                errorTitle.innerHTML = "Invalid username";
-                errorLbl.innerHTML = "Please enter a valid username";
-            } else {
-                errorTitle.innerHTML = "Invalid password";
-                errorLbl.innerHTML = "Please enter a valid password";
-            }
-        }
-    });
+    document.querySelector("#submit").addEventListener("click", handleAuth);
 });
