aa30334fe55a04c4b8f32baf1f225c82829d8cc4
sachintha180 <sachinthasenanayake180@gmail.com>
2023-12-23 09:27:37 +0530

    Refactored common JS functions to helpers.js

---
+++ static/scripts/helpers.js
@@ -0,0 +1,43 @@
+async function postJSON(routeURL, jsonObject) {
+    try {
+        const response = await fetch(routeURL, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(jsonObject),
+        });
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        return error;
+    }
+}
+
+function showError(errorMessage, errorAction, errorType="FlamesPOS Error") {
+    const errorTitle = document.querySelector("#error_title");
+    const errorSubtitle = document.querySelector("#error_type");
+    const errorLbl = document.querySelector("#error_lbl");
+
+    const errorModal = document.querySelector("#error");
+
+    errorTitle.innerHTML = errorMessage;
+    errorSubtitle.innerHTML = errorType;
+    errorLbl.innerHTML = errorAction;
+
+    errorModal.setAttribute("open", true);
+}
+
+function hideError() {
+    const errorTitle = document.querySelector("#error_title");
+    const errorSubtitle = document.querySelector("#error_type");
+    const errorLbl = document.querySelector("#error_lbl");
+    
+    const errorModal = document.querySelector("#error");
+
+    errorTitle.innerHTML = "";
+    errorSubtitle.innerHTML = "";
+    errorLbl.innerHTML = "";
+
+    errorModal.removeAttribute("open");
+}
